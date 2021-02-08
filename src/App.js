014e428c2d1304ec70ae235be91753d28a9335b0
Irina Barreto <abraeva.98@icloud.com>
2021-02-07 20:59:48 -0500

    provider

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import './App.css';
-import Car from './Car/Car';
-import React, { Component } from 'react';
-import Counter from './Counter/Counter'
+import "./App.css";
+import Car from "./Car/Car";
+import React, { Component } from "react";
+import Counter from "./Counter/Counter";
+
+export const ClickedContext = React.createContext(false);
 
 class App extends Component {
-
   state = {
     cars: [
-      {name: "Ford", year: 2018},
-      {name: "Audi", year: 2011},
-      {name: "Mazda", year: 2012},
+      { name: "Ford", year: 2018 },
+      { name: "Audi", year: 2011 },
+      { name: "Mazda", year: 2012 },
     ],
     pageTitle: "Super Star",
-    showCars: false
-  }
+    showCars: false,
+    clicked: false,
+  };
 
   onChangeName = (name, index) => {
     const car = this.state.cars[index];
@@ -21,61 +23,70 @@ class App extends Component {
     const cars = [...this.state.cars];
     cars[index] = car;
     this.setState({
-      cars
-    })
-  }
+      cars,
+    });
+  };
 
   changeShowCars = () => {
     this.setState({
-      showCars: !this.state.showCars
-    })
-  }
+      showCars: !this.state.showCars,
+    });
+  };
 
   deleteHandler = (index) => {
     const cars = this.state.cars.concat();
     cars.splice(index, 1);
-    this.setState({cars});
-  }
+    this.setState({ cars });
+  };
 
   // componentWillMount() {
   //   console.log('will mount')
   // }
 
   componentDidMount() {
-    console.log('did mount')
+    console.log("did mount");
   }
 
   render() {
-    console.log('render')
+    console.log("render");
     const cars = this.state.cars;
     return (
       <div className="App">
 
-        <Counter />
+        <ClickedContext.Provider value={this.state.clicked}>
+            <Counter />
+        </ClickedContext.Provider>
 
         <h1>{this.state.pageTitle}</h1>
 
-        <button 
-            style={{marginTop: '20px'}}
-            onClick={this.changeShowCars}>Show Cars</button>
+        <button style={{ marginTop: "20px" }} onClick={this.changeShowCars}>
+          Show Cars
+        </button>
 
-        <div style={{
-          width: 400,
-          margin: 'auto',
-          paddingTop: '20px'
-        }}>
-        {this.state.showCars && 
-              cars.map((car, index) => (
-                <Car 
-                    name={car.name} 
-                    year={car.year}
-                    key={index} 
-                    index={index}
-                    onChangeName={(event) => this.onChangeName(event.target.value, index)}
-                    onDelete={this.deleteHandler.bind(this, index)}
-                />
-              ))
-        }
+        <button onClick={() => this.setState({ clicked: true })}>
+          Change Clicked
+        </button>
+
+        <div
+          style={{
+            width: 400,
+            margin: "auto",
+            paddingTop: "20px",
+          }}
+        >
+          {this.state.showCars &&
+            cars.map((car, index) => (
+              <Car
+                name={car.name}
+                year={car.year}
+                key={index}
+                index={index}
+                onChangeName={(event) =>
+                  this.onChangeName(event.target.value, index)
+                }
+                onDelete={this.deleteHandler.bind(this, index)}
+              />
+            ))}
         </div>
       </div>
     );
